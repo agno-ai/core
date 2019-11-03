0ff2e569bb266b1f334aff20bb3be49c19023916
SydneyN <43929759+SydneyN@users.noreply.github.com>
2019-11-03 12:03:32 +0100

    Update db_tests.py

--- Database/Tests/db_tests.py
+++ Database/Tests/db_tests.py
@@ -1,9 +1,13 @@
-#Test connection to database server
+
 
 import mysql.connector
 from mysql.connector import Error
 from termcolor import colored
 import datetime
+
+
+#Test connection to database server
+
 
 
 print(colored("--------------Starting Connection Test-----------------","blue"))
@@ -26,6 +30,7 @@ try:
 except Error as e:
     print("Connection Test Failed", e)
     print(colored('Connection test---------- FAILED', 'red'))
+	
 finally:
     if (connection.is_connected()):
         cursor.close()
@@ -57,7 +62,46 @@ try:
 except Error as e:
     print("Credential Test Passed", e)
     print(colored('Credential Test--------- PASSED', 'green'))
+	
 finally:
     if (connection.is_connected()):
         cursor.close()
         connection.close()
+		
+        
+#Test selection from DB tables
+
+print(colored("\n--------------Starting SELECT Query Test-----------------","blue"))
+
+try:
+    connection = mysql.connector.connect(host='agno-db-dev.c2witq1chtnp.eu-central-1.rds.amazonaws.com',
+                                         database='agno_db',
+                                         user='agnoteam',
+                                         password='pw4agnodb')
+    if connection.is_connected():
+        sql_select_Query = "select * from Users"
+        cursor = connection.cursor()
+        cursor.execute(sql_select_Query)
+        records = cursor.fetchall()
+        print("Total number of records in Users table  is: ", cursor.rowcount)
+
+        print("\nPrinting each User record\n")
+        for row in records:
+            print("EntryNo --> ", row[0], )
+            print("accound_id --> ", row[1])
+            print("face_id --> ", row[2])
+            print("Timestamp --> ", row[3])
+            print("Emotion --> ", row[4], "\n")
+        
+        print(colored('SELECT Query test ------- PASSED', 'green'))
+            
+        
+
+except Error as e:
+    print("Error reading data from MySQL table", e)
+    print(colored('SELECT Query test ------- FAILED', 'red'))
+	
+finally:
+    if (connection.is_connected()):
+        connection.close()
+        cursor.close()
