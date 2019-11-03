308eb9c2d6b9629cef53d639c0b16deee3f23ee0
SydneyN <43929759+SydneyN@users.noreply.github.com>
2019-11-03 11:49:53 +0100

    Update db_tests.py

--- Database/Tests/db_tests.py
+++ Database/Tests/db_tests.py
@@ -2,8 +2,11 @@
 
 import mysql.connector
 from mysql.connector import Error
+from termcolor import colored
+import datetime
 
 
+print(colored("--------------Starting Connection Test-----------------","blue"))
 
 try:
     connection = mysql.connector.connect(host='agno-db-dev.c2witq1chtnp.eu-central-1.rds.amazonaws.com',
@@ -17,13 +20,44 @@ try:
         cursor = connection.cursor()
         cursor.execute("select database();")
         record = cursor.fetchone()
-        print("Database name: ", record)
+        print("Database name: ", record,)
+        print(colored('Connection test ------- PASSED', 'green'))
 
 except Error as e:
-    print("Error while connecting to MySQL", e)
+    print("Connection Test Failed", e)
+    print(colored('Connection test---------- FAILED', 'red'))
 finally:
     if (connection.is_connected()):
         cursor.close()
         connection.close()
-        print("MySQL connection is closed \n**********************************************************")
-          
+      
+        
+        
+		
+#Test connection using invalid credentials
+
+
+print(colored("\n--------------Starting Invalid credentials testing-----------------","blue"))
+
+try:
+    connection = mysql.connector.connect(host='agno-db-dev.c2witq1chtnp.eu-central-1.rds.amazonaws.com',
+                                         database='agno_db',
+                                         user='root',
+                                         password='')
+
+    if connection.is_connected():
+        db_Info = connection.get_server_info()
+        print("Successfully connected to MySQL Server version ", db_Info)
+        cursor = connection.cursor()
+        cursor.execute("select database();")
+        record = cursor.fetchone()
+        print("Database name: ", record,)
+        print(colored('Connection test ------- FAILED', 'red'))
+
+except Error as e:
+    print("Credential Test Passed", e)
+    print(colored('Credential Test--------- PASSED', 'green'))
+finally:
+    if (connection.is_connected()):
+        cursor.close()
+        connection.close()
