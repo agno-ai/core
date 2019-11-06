db5a24816441896c973d4c850885d831942fb40e
Anthony Akpenyi <anthony.akpenyi@axians.de>
2019-11-06 17:43:06 +0100

    sequelize db config

--- src/infra/database/models/index.js
+++ src/infra/database/models/index.js
@@ -2,9 +2,25 @@ const { ModelsLoader } = require('src/infra/sequelize');
 const Sequelize = require('sequelize');
 const { db: config } = require('config');
 
+let sequelize;
 if(config) {
-  const sequelize = new Sequelize(config);
-
+  if(config.ssl) {
+    sequelize = new Sequelize(config.database, config.username, config.password, {
+      host: 'agno-db-dev.c2witq1chtnp.eu-central-1.rds.amazonaws.com',
+      port: 3306,
+      logging: console.log,
+      maxConcurrentQueries: 100,
+      dialect: 'mysql',
+      dialectOptions: {
+        ssl:'Amazon RDS'
+      },
+      pool: { maxConnections: 5, maxIdleTime: 30},
+      language: 'en'
+    })
+  } else {
+    sequelize = new Sequelize(config);
+  }
+  console.log(sequelize);
   module.exports = ModelsLoader.load({
     sequelize,
     baseFolder: __dirname
