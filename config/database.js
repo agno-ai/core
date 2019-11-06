d3123ec794bf88e3cd48d2d73865114f17fe02ea
Anthony Akpenyi <anthony.akpenyi@axians.de>
2019-11-06 16:35:28 +0100

    fix environment config for aws deployment

--- config/database.js
+++ config/database.js
@@ -11,6 +11,10 @@ module.exports = {
     password: process.env.RDS_PASSWORD,
     database: process.env.RDS_DB_NAME,
     host: process.env.RDS_HOSTNAME,
-    port: process.env.RDS_PORT
+    port: process.env.RDS_PORT,
+    ssl: 'Amazon RDS',
+    dialectOptions: {
+      ssl: 'Amazon RDS'
+    }
   }
 };
