50c68dc995724f448069381fd473fe1885c9fb7f
Anthony Akpenyi <anthony.akpenyi@axians.de>
2019-11-06 16:10:25 +0100

    set env for rds database

--- config/database.js
+++ config/database.js
@@ -6,5 +6,11 @@ module.exports = {
     host: '127.0.0.1',
     //dialect: 'postgres'
   },
-  production: process.env.DATABASE_URL
+  production: {
+    username: process.env.RDS_USERNAME,
+    password: process.env.RDS_PASSWORD,
+    database: process.env.RDS_DB_NAME,
+    host: process.env.RDS_HOSTNAME,
+    port: process.env.RDS_PORT
+  }
 };
