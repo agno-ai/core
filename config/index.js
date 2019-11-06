60e58d4a6328c10619bfc27461031916e69a235e
Anthony Akpenyi <anthony.akpenyi@axians.de>
2019-11-06 16:17:20 +0100

    fix environment config for aws deployment

--- config/index.js
+++ config/index.js
@@ -6,7 +6,6 @@ const path = require('path');
 const ENV = process.env.NODE_ENV || 'development';
 
 const envConfig = require(path.join(__dirname, 'environments', ENV));
-console.log(envConfig);
 const dbConfig = loadDbConfig();
 
 const config = Object.assign({
@@ -14,7 +13,6 @@ const config = Object.assign({
   env: ENV,
   db: dbConfig
 }, envConfig);
-console.log(config);
 
 module.exports = config;
 
