e428b4296e7d27793bc201366fddaf89ac3fcf20
Anthony Akpenyi <anthony.akpenyi@axians.de>
2019-11-06 16:16:30 +0100

    fix environment config for aws deployment

--- config/index.js
+++ config/index.js
@@ -6,6 +6,7 @@ const path = require('path');
 const ENV = process.env.NODE_ENV || 'development';
 
 const envConfig = require(path.join(__dirname, 'environments', ENV));
+console.log(envConfig);
 const dbConfig = loadDbConfig();
 
 const config = Object.assign({
@@ -13,14 +14,11 @@ const config = Object.assign({
   env: ENV,
   db: dbConfig
 }, envConfig);
+console.log(config);
 
 module.exports = config;
 
 function loadDbConfig() {
-  if(process.env.DATABASE_URL) {
-    return process.env.DATABASE_URL;
-  }
-
   if(fs.existsSync(path.join(__dirname, './database.js'))) {
     return require('./database')[ENV];
   }
