1737a7eb2b154603baed5e9e8f86988daaf3dfe6
Anthony Akpenyi <anthony.akpenyi@axians.de>
2019-11-07 15:20:44 +0100

    update

--- config/database.js
+++ config/database.js
@@ -9,7 +9,7 @@ module.exports = {
   production: {
     username: process.env.RDS_USERNAME,
     password: process.env.RDS_PASSWORD,
-    database: process.env.RDS_DB_NAME,
+    database: process.env.RDS_DB,
     host: process.env.RDS_HOSTNAME,
     port: process.env.RDS_PORT,
     dialect: 'mysql',
